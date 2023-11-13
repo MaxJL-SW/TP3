8cd6bbcd85e35e0bfe9ee30c674bd8e49259d7d4
MaxJL-SW <maxjoly69@gmail.com>
2023-11-13 17:09:46 +0100

    Commit

---
+++ src/Controller/FeedbackController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Feedback;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\FeedbackType;
+
+class FeedbackController extends AbstractController
+{
+    #[Route('/feedback/{productName}', name: 'feedback')]
+    public function feedback(Request $request, string $productName): Response
+
+    {    
+
+        $feedback = new Feedback();
+        $form = $this->createForm(FeedbackType::class, $feedback);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->render('feedback/result.html.twig', [
+                'feedback' =>   $feedback,
+                'productName' => $productName,
+            ]);
+        }
+
+        return $this->render('feedback/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/products', name: 'products')]
+    public function index(): Response
+    {
+        $products = [
+            // Créez une liste de produits en dur
+            ['name' => 'Produit 1', 'price' => 100],
+            ['name' => 'Produit 2', 'price' => 200],
+            ['name' => 'Produit 3', 'price' => 200],
+            ['name' => 'Produit 4', 'price' => 200],
+            ['name' => 'Produit 4', 'price' => 200],
+        ];
+
+        return $this->render('feedback/products.html.twig', [
+            'products' => $products,
+        ]);
+    }
+    
+}
---
+++ src/Form/FeedbackType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Feedback;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class FeedbackType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('nomClient', TextType::class, [
+            'constraints' => [
+                new NotBlank(),
+            ],
+        ])
+            ->add('emailClient', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                ],
+            ])
+            ->add('noteProduit', ChoiceType::class, [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ]
+            ])
+            ->add('commentaires', TextType::class)
+            ->add('dateSoumission', DateTimeType::class, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Feedback::class,
+        ]);
+    }
+}
